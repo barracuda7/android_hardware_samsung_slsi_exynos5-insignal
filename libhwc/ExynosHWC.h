c9054e5ae81b5fbfe2464103e06028f6635cb90c
barracuda7 <barracudaking@gmail.com>
2015-03-01 18:38:41 +1100

    revert

--- libhwc/ExynosHWC.h
+++ libhwc/ExynosHWC.h
@@ -280,11 +280,11 @@ struct exynos5_hwc_composer_device_1_t {
 #ifdef HWC_SERVICES
 
 #define S3D_ERROR -1
-#define HDMI_TIMINGS_DEFAULT V4L2_DV_1080P60
-#define HDMI_TIMINGS_ERROR -1
+#define HDMI_PRESET_DEFAULT V4L2_DV_1080P60
+#define HDMI_PRESET_ERROR -1
 
-    int mHdmiDVTimings;
-    int mHdmiCurrentDVTimings;
+    int mHdmiPreset;
+    int mHdmiCurrentPreset;
     bool mHdmiResolutionChanged;
     bool mHdmiResolutionHandled;
 #if defined(S3D_SUPPORT)
